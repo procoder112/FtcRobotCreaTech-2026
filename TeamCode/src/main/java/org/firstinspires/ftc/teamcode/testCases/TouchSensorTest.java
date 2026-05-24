7ac267d00904d2336080a690a4c139ce52f0a721
procoder112 <pravrutshah@gmail.com>
2026-05-24 15:24:03 -0700

    The codes that move the robot Controls: Left Joystick:Forward, Backward, Left, and Right Right Joystick: Turn left and Right DO NOT TOUCH the triangle, square, or circle. they are testing buttons. PS4 button X is for Brake XBOX button A is for Brake

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testCases/TouchSensorTest.java
@@ -0,0 +1,36 @@
+package org.firstinspires.ftc.teamcode.testCases;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.TouchSensor;
+
+@TeleOp(name = "Touch Sensor Test", group = "Samples")
+public class TouchSensorTest extends LinearOpMode {
+
+    private TouchSensor touchSensor;
+
+    @Override
+    public void runOpMode() {
+        // Initialize the hardware variables. Note that the strings used here as parameters
+        // to 'get' must correspond to the names assigned during the robot configuration
+        // step.
+        touchSensor = hardwareMap.get(TouchSensor.class, "touch_sensor");
+
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
+
+        // Wait for the game to start (driver presses PLAY)
+        waitForStart();
+
+        // run until the end of the match (driver presses STOP)
+        while (opModeIsActive()) {
+            // Check if the touch sensor is pressed
+            if (touchSensor.isPressed()) {
+                telemetry.addData("Touch Sensor", "Is Pressed");
+            } else {
+                telemetry.addData("Touch Sensor", "Is Not Pressed");
+            }
+            telemetry.update();
+        }
+    }
+}
